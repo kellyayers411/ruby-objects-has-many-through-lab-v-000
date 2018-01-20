36b6822dbb1a9ae0437e3ee488772b6c83a3f8b3
K Logic <kellyayers411@gmail.com>
2018-01-20 23:03:05 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -8,6 +8,17 @@ end
 
 def add_appointment(appointment)
   @appointments << appointment
+  appointment.patient = self
+end
+
+def appointments
+  @appointments
+end
+
+def doctors
+  @appointments.collect do |appointment|
+    appointment.doctor
+  end
 end
 
 end
